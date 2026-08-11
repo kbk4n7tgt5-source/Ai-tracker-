dd66893fc9d69a4dbc6189a7acb981da90bcdb7f
kbk4n7tgt5-source <kbk4n7tgt5@privaterelay.appleid.com>
2026-08-11 11:31:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from collections import deque
 from statistics import mean
 import requests
 
-app aapp = Flask(__name__, template_folder=".")
+app = Flask(__name__, template_folder=".")
 
 PRODUCT_ID = "BTC-USD"
 FAST_WINDOW = 6
